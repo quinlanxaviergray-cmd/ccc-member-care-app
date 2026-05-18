0dc39f493b5199624c888cce65694679b596c221
quin <quinlanxaviergray@gmail.com>
2026-05-18 13:13:53 -0400

    fix deployment

--- utils/supabase-admin.ts
+++ utils/supabase-admin.ts
@@ -5,5 +5,11 @@ export function createClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
   const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-  return createSupabaseClient(supabaseUrl, supabaseServiceKey)
+  return createSupabaseClient(supabaseUrl, supabaseServiceKey, {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
+    },
+  })
 }
