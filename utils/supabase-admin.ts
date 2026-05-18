6fe73dec79a0bfd59495a51e660d4c33387731ad
quin <quinlanxaviergray@gmail.com>
2026-05-18 12:53:35 -0400

    version 7.1

---
+++ utils/supabase-admin.ts
@@ -0,0 +1,9 @@
+// utils/supabase-admin.ts
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+
+  return createSupabaseClient(supabaseUrl, supabaseServiceKey)
+}
