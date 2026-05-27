2abf2a1db1232f70f69ce419000ae69d3740df4b
quin <quinlanxaviergray@gmail.com>
2026-05-27 19:42:14 -0400

    new favicon for iphone

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import './globals.css'
 export const metadata = {
   title: 'BurdenBear',
   description: 'Church member care tracker',
+    icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
